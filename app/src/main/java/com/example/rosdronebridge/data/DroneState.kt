39be1444f892dc99b6a0ef0331c79c0d85bc3282
markokova <marko.kovacevic1112@gmail.com>
2026-08-29 13:43:46 +0200

    Add home location distance params to DroneState

--- app/src/main/java/com/example/rosdronebridge/data/DroneState.kt
+++ app/src/main/java/com/example/rosdronebridge/data/DroneState.kt
@@ -3,6 +3,7 @@ package com.example.rosdronebridge.data
 import dji.sdk.keyvalue.value.flightcontroller.FCFlightMode
 import dji.sdk.keyvalue.value.flightcontroller.FCMotorStartFailureError
 import dji.sdk.keyvalue.value.flightcontroller.FlightMode
+import dji.sdk.keyvalue.value.flightcontroller.HomeLocationType
 
 data class DroneState(
     val connected: Boolean = false,
@@ -14,17 +15,20 @@ data class DroneState(
     val isHomeLocationSet: Boolean = false,
     val isCompassInNormalState: Boolean = false,
     val batteryChargeRemaining: Int? = 0,
-    val latitude: Double = 0.0,
-    val longitude: Double = 0.0,
-    val altitude: Double = 0.0,
+    val latitude: Double? = 0.0,
+    val longitude: Double? = 0.0,
+    val altitude: Double? = 0.0,
     val velocityX: Double = 0.0,
     val velocityY: Double = 0.0,
     val velocityZ: Double = 0.0,
-    val takeoffError: FCMotorStartFailureError? = null, // Track why takeoff fails
+    val takeoffError: FCMotorStartFailureError? = null,
     val positionX: Float = 0.0F,
     val positionY: Float = 0.0F,
     val positionZ: Float = 0.0F,
     val gimbalPitch: Double = 0.0,
     val gimbalRoll: Double = 0.0,
-    val gimbalYaw: Double = 0.0
+    val gimbalYaw: Double = 0.0,
+    val latitudeDistFromHome: Double? = 0.0,
+    val longitudeDistFromHome: Double? = 0.0,
+    val homeLocationType: HomeLocationType
 )
